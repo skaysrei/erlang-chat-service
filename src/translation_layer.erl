5f8a47122415ad62b2f5e8497e697562cb7ca4ed
skaysrei <alerocco90@gmail.com>
2023-02-07 20:17:08 +0100

    translation_layer command update

--- src/translation_layer.erl
+++ src/translation_layer.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %% @doc 
-%% translation layer worker.
+%% translation layer worker;
 %% receives the raw message from the client and parses it,
 %% sends the appropriate reques to the chat_controller.
 %% @end
@@ -33,9 +33,7 @@ handle_call({message, Msg}, _From, State) ->
     Parsed = parse_message(Msg),
     case Parsed of
         {Command, Content} ->
-            io:format("\nTranslation layer (Parsed data):\n\nCommand[ ~s ]\nMessage[ ~s ]\n\n", [Command, Content]),
-            %%  gen_server:cast(chat_controller, PID tcp worker, command/msg)
-            command_select({Command, Content} = Parsed, Pid),
+            command_select(Parsed, Pid),
             {reply, ok, State};
         protocol_error ->
             {reply, {error, "Err: Wrong protocol syntax OwO"}, State};
@@ -44,16 +42,6 @@ handle_call({message, Msg}, _From, State) ->
         _ ->
             {reply, error, State}
     end;
-
-
-
-    % case Command of
-    %     "LOGIN" ->
-    %         try_login(Content);
-    %     _ ->
-    %         {reply, {error, "Err: Unknown command UwU"}, State}
-    % end;
-
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -76,7 +64,7 @@ parse_message(Msg) ->
             protocol_error;
         true -> 
             {Command, [_|Content]} = lists:splitwith(fun(T) -> [T] =/= ":" end, Msg),
-            io:format("\n\nCONTENT: ~p", [Content]),
+            io:format("\n\nParsed command: C[~p] M[~p]\n\n", [Command, Content]),
             {Command, Content};
         _ ->
             unknown_error
@@ -85,16 +73,32 @@ parse_message(Msg) ->
 quit(Command, Msg) ->
     ok.
 
-%% TODO: change from pid to name
+%% TODO: change from pid to name, CHANGE TO gen_server:cast()
 command_select({"LOGIN", User}, ServerPid) ->
-    io:format("\n\nCOMMAND SELECTOR REACHED~n~n", []),
     gen_server:call(chat_controller, {login, User, ServerPid}),
     ok;
-command_select(Command, Msg) ->
+command_select({"LOGOUT", _}, ServerPid) ->
+    io:format("\n\nLOGOUT SELECTOR REACHED\n\n", []), %% remove
+    gen_server:call(chat_controller, {logout, ServerPid}),
     ok;
-command_select(Command, Msg) ->
+
+command_select({"NEWROOM", User}, ServerPid) ->
+    gen_server:cast(chat_controller, {newroom, User, ServerPid}),
     ok;
-command_select(Command, Msg) ->
+command_select({"DELROOM", User}, ServerPid) ->
+    gen_server:cast(chat_controller, {delroom, User, ServerPid}),
+    ok;
+command_select({"LISTROOM", User}, ServerPid) ->
+    gen_server:cast(chat_controller, {listroom, User, ServerPid}),
+    ok;
+command_select({"JOINROOM", User}, ServerPid) ->
+    gen_server:cast(chat_controller, {joinroom, User, ServerPid}),
+    ok;
+command_select({"EXITROOM", User}, ServerPid) ->
+    gen_server:cast(chat_controller, {exitroom, User, ServerPid}),
+    ok;
+
+command_select({"SAY", User}, ServerPid) ->
     ok.
 
 try_login(User) ->
