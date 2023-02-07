0fb105af06331909c51535a50c2ad4e3583427cd
skaysrei <alerocco90@gmail.com>
2023-02-07 19:51:26 +0100

    tcp_server worker cleanup

--- src/tcp_server.erl
+++ src/tcp_server.erl
@@ -1,3 +1,11 @@
+%%%-------------------------------------------------------------------
+%% @doc 
+%% gen_tcp server to handle client-server data exchange;
+%% accepts for incoming connections and forwards data to
+%% translation_layer, while sending back to the cliet the
+%% messages received by the chat_controller.
+%% @end
+%%%-------------------------------------------------------------------
 -module(tcp_server).
 -behaviour(gen_server).
 
@@ -7,26 +15,24 @@
 -record(state, {socket, trs_pid}).
 
 start_link(Socket) ->
-	{ok, Pid} = gen_server:start_link(?MODULE, Socket, []),
-	io:format("TEST PID1 ~p\n", [Pid]),
-	{ok, Pid}.
-
+	gen_server:start_link(?MODULE, Socket, []).
 
 init(Socket) ->
-	%% Start accepting requests
-	%% We must cast this to the worker's process, as it blocks it.
+	%% start accepting requests;
+	%% we must cast this to the worker's process, as it blocks it.
 	gen_server:cast(self(), accept),
 	{ok, #state{socket=Socket}}.
 
-%% enters accepting state on the socket
-%% will handle async messages from gen_tcp
+
 handle_cast(accept, State = #state{socket=ListenSocket}) ->
+	%% enters accepting state on the socket
 	{ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
 	%% boot a new listener to replace this one.
 	networking_sup:start_server_worker(),
 	{ok, Pid} = translation_layer:start_link(),
-	send(AcceptSocket, "Server ready :3", []),
+	send(AcceptSocket, "Server ready.", []),
 	{noreply, State#state{socket=AcceptSocket, trs_pid=Pid}};
+%% handles chat_controller output
 handle_cast(login, State) ->
 	send(State#state.socket, "Logged in successfully OwO", []),
 	{noreply, State};
@@ -37,16 +43,15 @@ handle_cast({message, Message}, State) ->
 	send(State#state.socket, Message, []),
 	{noreply, State}.
 
-%% handles messages from the client
-handle_info({tcp, Socket, "quit"++_}, State) ->
+%% handling of requests from gen_tcp---------------------------------
+handle_info({tcp, Socket, "!exit"++_}, State) ->
 	gen_tcp:close(Socket),
 	{stop, normal, State};
-%% handles the message received on the socket
 handle_info({tcp, Socket, Msg}, State) ->
-	%% send received msg to chat_controller
+	%% TODO: change to gen_server:cast
 	gen_server:call(State#state.trs_pid, {message, Msg}),
-	send(Socket, "Server received: " ++ Msg, []),
 	{noreply, State};
+%% TODO add logout for conn lost or error
 handle_info({tcp_closed, _Socket}, State) -> {stop, normal, State};
 handle_info({tcp_error, _Socket, _}, State) -> {stop, normal, State};
 handle_info(E, State) ->
@@ -60,9 +65,9 @@ handle_call({error, Msg}, _From, State) ->
 terminate(_Reason, _Tab) -> ok.
 code_change(_OldVersion, Tab, _Extra) -> {ok, Tab}.
 
-%% Send a message back to the client
+%% send message back to the client
 send(Socket, Str, Args) ->
-	%% TODO: Remove complexity
-	ok = gen_tcp:send(Socket, io_lib:format(Str++"~n", Args)),
+	%% TODO: Overhaul this thing
+	ok = gen_tcp:send(Socket, io_lib:format(Str, Args)),
 	ok = inet:setopts(Socket, [{active, once}]),
 	ok.
